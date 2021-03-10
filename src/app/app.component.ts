029c301186da6c80298993f8f6328ddc8d471ddb
kajal.Ramani <kajal.ramani@thegatewaycorp.com>
2021-03-10 14:55:47 +0530

    add ChildView

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
+
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Home'
+
+
+export class AppComponent implements AfterViewInit {
+  
+  title = 'Home';
+
+  @ViewChild(ProductComponent) proCmp!: ProductComponent;
+
+  //@ViewChild('proCmp') prCmp!: ProductComponent;
+
+  ngAfterViewInit(){
+    this.proCmp.floorNumber = '4th';
+  }
+
 }
